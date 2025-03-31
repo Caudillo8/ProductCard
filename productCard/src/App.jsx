6d4f5567449124468c533a768a0d36c5323fb3d5
hernandez matias <matias04hernandez5329@gmail.com>
2025-03-31 00:27:35 -0300

    fetch
    probando console log

--- productCard/src/App.jsx
+++ productCard/src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 //import './App.css'
-import{Card} from './components/Card'
+import { Card } from "./components/Card";
 function App() {
-
-
-  return (
-<Card></Card>
-  )
+  return <Card nombre="Smart TV 42 Sansei Android"></Card>;
 }
 
-export default App
+export default App;
